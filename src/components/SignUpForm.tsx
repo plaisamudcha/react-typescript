199746efd98eeb919c96523c51b5f9ff9c94fd2b
plaisamudcha <plai_samudcha@hotmail.com>
2025-10-15 15:00:31 +0700

    implement onChange input signup

--- src/components/SignUpForm.tsx
+++ src/components/SignUpForm.tsx
@@ -1,13 +1,49 @@
+import {
+  useState,
+  type ChangeEventHandler,
+  type FormEventHandler,
+} from "react";
 import Button from "./ui/Button";
 import Form from "./ui/Form";
 import Input from "./ui/Input";
 
+type FormInput = {
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
+
+const initialInput: FormInput = {
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
+
 export default function SignUpForm() {
+  //   const [loading, setLoading] = useState<boolean>(false);
+  const [formInput, setFormInput] = useState<FormInput>(initialInput);
+
+  const handleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    const { name, value } = e.target;
+    if (name) {
+      setFormInput((prev) => ({ ...prev, [name]: value }));
+    }
+  };
+
+  const handleSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <div className="flex flex-col gap-1">
         <label htmlFor="email">Email</label>
-        <Input placeholder="Enter Email" name="email" id="email" />
+        <Input
+          placeholder="Enter Email"
+          name="email"
+          id="email"
+          onChange={handleChange}
+        />
       </div>
       <div className="flex flex-col gap-1">
         <label htmlFor="password">Password</label>
@@ -16,6 +52,7 @@ export default function SignUpForm() {
           name="password"
           id="password"
           type="password"
+          onChange={handleChange}
         />
       </div>
       <div className="flex flex-col gap-1">
@@ -25,10 +62,11 @@ export default function SignUpForm() {
           name="confirmPassword"
           id="confirmPassword"
           type="password"
+          onChange={handleChange}
         />
       </div>
       <div className="mt-2">
-        <Button>
+        <Button className="w-full">
           <span className="font-medium">Create account</span>
         </Button>
       </div>
